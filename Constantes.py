ef0b11e9c3712fa79681309cbc3fbf43a7f55d14
Theo Servotte <theo.servotte.etu@univ-lille.fr>
2024-02-19 21:41:08 +0100

    Update 2 files
    - /EDO.py
    - /Constantes.py

--- Constantes.py
+++ Constantes.py
@@ -1,3 +1,40 @@
-def test():
-    return 0
-    
+#Fichier contenant toutes nos constantes
+
+
+#Geometry
+Rcell = 8 #µM
+fR = 0.25
+fV = 0.01
+fA = 30
+Cm = 28 #fF/µm^2
+
+#Ions and potentials:
+T = 310 #Kelvin
+V0 = -60 #mV
+V_ER0 = -60
+C0 = 0.1 #µM
+C_ER0 = 0.4 # mM
+C_ext = 2 #mM
+delta_V_C = 78 #mV
+delta_V_C_ER = 63 #mv
+
+#Calcium buffer: µM
+b0 = 100
+Kb = 0.1
+b_ER0 = 30
+K_ERb = 0.1
+
+#Second messengers: nM
+P0 = 8.7
+beta_p = 0.6 #nM/s
+gamma_p = 0.01149 #nM/s
+Cp = 0.5 #µM
+n_p = 1
+
+#Densités: µm^2
+rho_IP3R = 11.35
+rho_SERCA = 700
+rho_PMCA = 68.57
+rho_CRAC0 = 0.6
+rho_CRAC_pos = 3.9
+rho_CRAC_neg = 0.5115
--- EDO.py
+++ EDO.py
@@ -1,3 +1,5 @@
+import Constantes
+
 def test():
     return 
     
