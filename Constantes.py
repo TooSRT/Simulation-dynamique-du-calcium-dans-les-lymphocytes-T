b81839eff9508b74b8a45f67cd9a240602af8356
Théo <theo.servotte.etu@univ-lille.fr>
2024-02-26 23:39:57 +0100

    màj EDO + cte

--- Constantes.py
+++ Constantes.py
@@ -6,7 +6,9 @@ from scipy.integrate import solve_ivp
 from scipy import constants
 import matplotlib.pyplot as plt
 
+#Numérotation correspondent à celles de l'article 
 
+#-------Constantes tableau------
 #Geometry
 Rcell = 8 #µM
 fR = 0.25
@@ -15,8 +17,8 @@ fA = 30
 Cm = 28 #fF/µm^2
 
 #Ions and potentials:
-T = 310 #Kelvin
-V0 = -60 #mV
+Temp = 310 #Kelvin
+V0 = V_ER = -60 #mV #V=V0=V_ER
 V_ER0 = -60
 C0 = 0.1 #µM
 C_ER0 = 0.4 # mM
@@ -45,10 +47,6 @@ rho_CRAC0 = 0.6
 rho_CRAC_pos = 3.9
 rho_CRAC_neg = 0.5115
 
-
-Far_cte = 96485.33212 #Faraday constant C mol^-1
-zCA = 2
-
 #--------Fonctions--------
 
 def Hill_function(X,K,n): #fonction de hill (8)
@@ -61,20 +59,32 @@ def fC(b0,C,Kb):  #fraction of free calcium (3)
     return 1 / ( 1 + b0/(C+Kb) )
 
 #-------Déterminations de constantes-----
+Faraday = 96485.33212 #Faraday constant C mol^-1
+zCA = 2
+V_C_barre = 50 #(9)
 
 I_SERCA = 3.10**(-6) * Hill_function(C, 0.4, 2) #(32)
 I_PMCA = 10**(-5)* g_PMCA
-#I_CRAC =    #(23)
+I_CRAC = 2*(V0 - V_C_barre)    #(23) car V=V0
+
 
 Vcyt = 4/3 * np.pi * Rcell**3 * (1-fV-fR**3) #(20)
 V_ER_tilde = 4/3 * np.pi * Rcell**3 *fV #(21)
 A_ER = 4*np.pi*fA*(3*V_ER_tilde/4*np.pi)**(2/3) #(22)
 
-Xi= 804.2/Vcyt #(16)
+rho_CRAC_barre = rho_CRAC_neg  + (rho_CRAC_pos - rho_CRAC_neg)*(1-Hill_function('C_ER',169, 4.2)) #(25) #C_er varie et définie par dCER_dt
+
+#V_C_ER_barre = 8.315*Temp*np.ln(C_ER/C)/(zCA*Faraday) - delta_V_C_ER
+
+
+
+Xi = 804.2/Vcyt #(16)
 Xi_ER = A_ER/Vcyt   #(17)
-XI_ERC = A_ER/V_ER_tilde #(19)
+Xi_ERC = A_ER/V_ER_tilde #(19)
 
 g_IP3R = 0.81 * Hill_function(C, 0,21, 1.9) # (27) cte dispo sur le papier
-C_IP3R_inh = 52*H(P, 0.05, 4)
+C_IP3R_inh = 52 * Hill_function(P, 0.05, 4)
 h_IP3R = Hill_function(C_IP3R_inh, C, 3.9)
+I_IP3R = 0.064*g_IP3R*h_IP3R*(V0 - V_ER - V_C_ER_barre)
+
 
--- EDO.py
+++ EDO.py
@@ -3,17 +3,72 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
+
+#Numérotation correspondent à celles de l'article 
 #-------EDO-------
 
-def Cytoclosic_calcium(t): #Evolution de calcium cytosolic (1)
-    B_C=BC(b0,Kb,dC_dt)
-    dC_dt = -1/(zCA*(Far_cte*(1 + B_C))) * (Xi*rho_PMCA*I_PMCA + Xi*rho_CRAC*I_CRAC + Xi_ERC*rho_SERCA*I_SERCA + Xi_ERC*rho_IP3R*I_IP3R)
-    return dC_dt
+def dC_dt(t,Y): #Evolution de calcium cytosolic (1)
+    C = Y
+    #g_pmca, P, rho_crac = Y car variables, utiliser leurs edo dans la fonction ?
+    B_C = BC(b0,Kb,C)
+    dC = -1/(zCA*(Faraday*(1 + B_C))) * (Xi*rho_PMCA*I_PMCA + Xi*rho_CRAC*I_CRAC + Xi_ERC*rho_SERCA*I_SERCA + Xi_ERC*rho_IP3R*I_IP3R)
+    return dC
 
-def C_ER(t): #dynamics of the calcium concentration in the ER (4)
+def dCER_dt(t,Y): #dynamics of the calcium concentration in the ER (4)
+    C_ER = Y
     B_CER = BC(b_ER0,K_ERb,C_ER)
-    dCer_dt = Xi_ER*(rho_SERCA*I_SERCA +rho_IP3R*I_IP3R)/(zCA*(Far_cte*(1 + B_CER)))
-    return dCer_dt 
+    dCer = Xi_ER*(rho_SERCA*I_SERCA +rho_IP3R*I_IP3R)/(zCA*(Faraday*(1 + B_CER)))
+    return dCer
 
-def IP3(t): #(7)
+def dP_dt(t,Y): #(7)
     return 0
+
+def drho_CRAC_dt(t,Y): #(24)
+    rho_CRAC = Y
+    tau_CRAC = 5 #à revoir
+    drho_CRAC = (rho_CRAC_barre - rho_CRAC )/ tau_CRAC
+    return drho_CRAC
+
+def dg_IP3R_dt(t,Y): #(29)
+    g_IP3R, C = Y
+    g_IP3R_max = 0.81
+    C_IP3R_act = 0.21
+    n_IP3R_act = 1.9
+    tau_IP3R = 100 # compris entre 1sec et 100ms
+    dg_IP3R = (g_IP3R_max*Hill_function(C,C_IP3R_act,n_IP3R_act) - g_IP3R) /tau_IP3R
+    return dg_IP3R
+
+def dh_IP3R_dt(t,Y): #(29)
+    C = Y #rajouter P ?
+    theta = 300
+    n_IP3R_inh = 3.9
+    dh_IP3R = (Hill_function(C_IP3R_inh, C, n_IP3R_inh) - h_IP3R)/theta #définir c_IP3R dans la fonction car variable avec P ?
+    return dh_IP3R
+
+def dg_PMCA_dt(t,Y): # (31)
+    return 0
+
+
+
+#--------Graphiques-------
+
+#Plage de temps
+time =10
+
+#Résolution de l'équation différentielle
+#C0 condition initiale
+sol = solve_ivp(dC_dt, [0, time], C0, args=(), dense_output=True) #g_PMCA, P; rho_crac etc varient
+
+# Temps pour l'interpolation
+t = np.linspace(0, 10, 300)
+
+# Interpolation de la solution
+C_interp = sol.sol(t)
+
+# Affichage
+plt.plot(t, C_interp[0], label='C')
+plt.xlabel('Temps')
+plt.ylabel('Concentration en calcium')
+plt.legend()
+plt.title('Evolution de la concentration en Ca2+ au cour du temps')
+plt.show()
