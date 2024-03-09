497a1a5fd2849906e483d342af64d3895a749c5a
Théo <theo.servotte.etu@univ-lille.fr>
2024-03-09 11:24:20 +0100

    modif système d'EDO

--- Constantes.py
+++ Constantes.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 #Numérotation correspondent à celles de l'article 
 
-#-------Constantes tableau------
+#-------Constantes du tableau------
 #Geometry
 Rcell = 8 #µM
 fR = 0.25
@@ -58,33 +58,65 @@ def BC(b,K,C): #cytosolic calcium-buffer (2)
 def fC(b0,C,Kb):  #fraction of free calcium (3)
     return 1 / ( 1 + b0/(C+Kb) )
 
-#-------Déterminations de constantes-----
+#-------Déterminations de constantes--------
 Faraday = 96485.33212 #Faraday constant C mol^-1
 zCA = 2
 V_C_barre = 50 #(9)
-
-I_SERCA = 3.10**(-6) * Hill_function(C, 0.4, 2) #(32)
-I_PMCA = 10**(-5)* g_PMCA
-I_CRAC = 2*(V0 - V_C_barre)    #(23) car V=V0
-
 
 Vcyt = 4/3 * np.pi * Rcell**3 * (1-fV-fR**3) #(20)
 V_ER_tilde = 4/3 * np.pi * Rcell**3 *fV #(21)
 A_ER = 4*np.pi*fA*(3*V_ER_tilde/4*np.pi)**(2/3) #(22)
 
-rho_CRAC_barre = rho_CRAC_neg  + (rho_CRAC_pos - rho_CRAC_neg)*(1-Hill_function('C_ER',169, 4.2)) #(25) #C_er varie et définie par dCER_dt
-
-#V_C_ER_barre = 8.315*Temp*np.ln(C_ER/C)/(zCA*Faraday) - delta_V_C_ER
-
-
-
 Xi = 804.2/Vcyt #(16)
 Xi_ER = A_ER/Vcyt   #(17)
 Xi_ERC = A_ER/V_ER_tilde #(19)
 
-g_IP3R = 0.81 * Hill_function(C, 0,21, 1.9) # (27) cte dispo sur le papier
-C_IP3R_inh = 52 * Hill_function(P, 0.05, 4)
-h_IP3R = Hill_function(C_IP3R_inh, C, 3.9)
-I_IP3R = 0.064*g_IP3R*h_IP3R*(V0 - V_ER - V_C_ER_barre)
+#Mettre tous les paramtrères de la fonction
+def dC_dt(t,Y0,Y1,Y2,Y3,Y4,Y5,Y6): 
+    #-------Variables du système-------
+    C = Y0
+    C_ER = Y1
+    P = Y2
+    rho_CRAC = Y3
+    g_IP3R = Y4
+    h_IP3R = Y5
+    g_PMCA = Y6
+
+    #--------Initialisation de différentes fonctions/paramètres qui dépendent de nos variables--------
+    B_C = BC(b0,Kb,C)
+    B_CER = BC(b_ER0,K_ERb,C_ER)
+    I_SERCA = 3.10**(-6) * Hill_function(C, 0.4, 2) #(32)
+    I_PMCA = 10**(-5)* g_PMCA #(30)
+    I_CRAC = 2*(V0 - V_C_barre)    #(23) car V=V0
+    V_C_ER_barre = 8.315*Temp*np.ln(C_ER/C)/(zCA*Faraday) - delta_V_C_ER
+    rho_CRAC_barre = rho_CRAC_neg  + (rho_CRAC_pos - rho_CRAC_neg)*(1-Hill_function(C_ER,169, 4.2)) #(25) 
+    g_IP3R = 0.81 * Hill_function(C, 0,21, 1.9) # (27) Constante dispo sur le papier
+    C_IP3R_inh = 52 * Hill_function(P, 0.05, 4)
+    h_IP3R = Hill_function(C_IP3R_inh, C, 3.9)
+    I_IP3R = 0.064*g_IP3R*h_IP3R*(V0 - V_ER - V_C_ER_barre)
+
+    #--------Constantes--------
+    g_IP3R_max = 0.81
+    C_IP3R_act = 0.21
+    n_IP3R_act = 1.9
+    tau_IP3R = 100 # compris entre 1sec et 100ms
+    
+    theta = 300
+    n_IP3R_inh = 3.9
+
+    C_PMCA = 0,1 #µM
+    tau_PMCA = 50 #s
+
+    #--------Système d'ODE--------
+    dC_dt = -1/(zCA*(Faraday*(1 + B_C))) * (Xi*rho_PMCA*I_PMCA + Xi*rho_CRAC*I_CRAC + Xi_ERC*rho_SERCA*I_SERCA + Xi_ERC*rho_IP3R*I_IP3R)
+    dC_ER_dt = Xi_ER*(rho_SERCA*I_SERCA +rho_IP3R*I_IP3R)/(zCA*(Faraday*(1 + B_CER)))       # (4)
+    dP_dt = beta_p * Hill_function(C,Cp,n_p)*T(t) - gamma_p*P         # (7)  T(t)=1 ?
+    drho_CRAC_dt = (rho_CRAC_barre - rho_CRAC )/ 5      #(24)
+    dg_IP3R_dt = (g_IP3R_max*Hill_function(C,C_IP3R_act,n_IP3R_act) - g_IP3R) /tau_IP3R      # (29)
+    dh_IP3R_dt = (Hill_function(C_IP3R_inh, C, n_IP3R_inh) - h_IP3R)/theta      # (29)
+    dg_PMCA_dt = (Hill_function(C,C_PMCA,2) - g_PMCA)/tau_PMCA      # (31)
+    
+    return 0
+
 
 
--- EDO.py
+++ EDO.py
@@ -7,48 +7,52 @@ import matplotlib.pyplot as plt
 #Numérotation correspondent à celles de l'article 
 #-------EDO-------
 
-def dC_dt(t,Y): #Evolution de calcium cytosolic (1)
-    C = Y
-    #g_pmca, P, rho_crac = Y car variables, utiliser leurs edo dans la fonction ?
+#Mettre tous les paramtrères de la fonction
+def dC_dt(t,Y0,Y1,Y2,Y3,Y4,Y5,Y6): 
+    #-------Variables du système-------
+    C = Y0
+    C_ER = Y1
+    P = Y2
+    rho_CRAC = Y3
+    g_IP3R = Y4
+    h_IP3R = Y5
+    g_PMCA = Y6
+
+    #--------Initialisation de différentes fonctions/paramètres qui dépendent de nos variables--------
     B_C = BC(b0,Kb,C)
-    dC = -1/(zCA*(Faraday*(1 + B_C))) * (Xi*rho_PMCA*I_PMCA + Xi*rho_CRAC*I_CRAC + Xi_ERC*rho_SERCA*I_SERCA + Xi_ERC*rho_IP3R*I_IP3R)
-    return dC
-
-def dCER_dt(t,Y): #dynamics of the calcium concentration in the ER (4)
-    C_ER = Y
     B_CER = BC(b_ER0,K_ERb,C_ER)
-    dCer = Xi_ER*(rho_SERCA*I_SERCA +rho_IP3R*I_IP3R)/(zCA*(Faraday*(1 + B_CER)))
-    return dCer
+    I_SERCA = 3.10**(-6) * Hill_function(C, 0.4, 2) #(32)
+    I_PMCA = 10**(-5)* g_PMCA #(30)
+    I_CRAC = 2*(V0 - V_C_barre)    #(23) car V=V0
+    V_C_ER_barre = 8.315*Temp*np.ln(C_ER/C)/(zCA*Faraday) - delta_V_C_ER
+    rho_CRAC_barre = rho_CRAC_neg  + (rho_CRAC_pos - rho_CRAC_neg)*(1-Hill_function(C_ER,169, 4.2)) #(25) 
+    g_IP3R = 0.81 * Hill_function(C, 0,21, 1.9) # (27) Constante dispo sur le papier
+    C_IP3R_inh = 52 * Hill_function(P, 0.05, 4)
+    h_IP3R = Hill_function(C_IP3R_inh, C, 3.9)
+    I_IP3R = 0.064*g_IP3R*h_IP3R*(V0 - V_ER - V_C_ER_barre)
 
-def dP_dt(t,Y): #(7)
-    return 0
-
-def drho_CRAC_dt(t,Y): #(24)
-    rho_CRAC = Y
-    tau_CRAC = 5 #à revoir
-    drho_CRAC = (rho_CRAC_barre - rho_CRAC )/ tau_CRAC
-    return drho_CRAC
-
-def dg_IP3R_dt(t,Y): #(29)
-    g_IP3R, C = Y
+    #--------Constantes--------
     g_IP3R_max = 0.81
     C_IP3R_act = 0.21
     n_IP3R_act = 1.9
     tau_IP3R = 100 # compris entre 1sec et 100ms
-    dg_IP3R = (g_IP3R_max*Hill_function(C,C_IP3R_act,n_IP3R_act) - g_IP3R) /tau_IP3R
-    return dg_IP3R
-
-def dh_IP3R_dt(t,Y): #(29)
-    C = Y #rajouter P ?
+    
     theta = 300
     n_IP3R_inh = 3.9
-    dh_IP3R = (Hill_function(C_IP3R_inh, C, n_IP3R_inh) - h_IP3R)/theta #définir c_IP3R dans la fonction car variable avec P ?
-    return dh_IP3R
 
-def dg_PMCA_dt(t,Y): # (31)
+    C_PMCA = 0,1 #µM
+    tau_PMCA = 50 #s
+
+    #--------Système d'ODE--------
+    dC_dt = -1/(zCA*(Faraday*(1 + B_C))) * (Xi*rho_PMCA*I_PMCA + Xi*rho_CRAC*I_CRAC + Xi_ERC*rho_SERCA*I_SERCA + Xi_ERC*rho_IP3R*I_IP3R)
+    dC_ER_dt = Xi_ER*(rho_SERCA*I_SERCA +rho_IP3R*I_IP3R)/(zCA*(Faraday*(1 + B_CER)))       # (4)
+    dP_dt = beta_p * Hill_function(C,Cp,n_p)*T(t) - gamma_p*P         # (7)  T(t)=1 ?
+    drho_CRAC_dt = (rho_CRAC_barre - rho_CRAC )/ 5      #(24)
+    dg_IP3R_dt = (g_IP3R_max*Hill_function(C,C_IP3R_act,n_IP3R_act) - g_IP3R) /tau_IP3R      # (29)
+    dh_IP3R_dt = (Hill_function(C_IP3R_inh, C, n_IP3R_inh) - h_IP3R)/theta      # (29)
+    dg_PMCA_dt = (Hill_function(C,C_PMCA,2) - g_PMCA)/tau_PMCA      # (31)
+    
     return 0
-
-
 
 #--------Graphiques-------
 
